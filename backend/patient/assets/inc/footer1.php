fd3e941601863af4f388a909077b6b1f2acdcc72
BhavikaPachauri <pachauribhavika2000@gmail.com>
2024-07-09 12:24:11 +0530

    Initial commit

---
+++ backend/patient/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt fw-bold">
+    2023 - <?php echo date('Y'); ?> &copy; Cardio Care</a>
+</footer>
---
+++ backend/patient/assets/inc/sidebar.php
@@ -0,0 +1,103 @@
+<div class="left-side-menu">
+
+    <div class="slimscroll-menu">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <ul class="metismenu" id="side-menu">
+
+                <li class="menu-title">Navigation</li>
+
+                <li>
+                    <a href="his_admin_dashboard.php">
+                        <i class="fe-airplay"></i>
+                        <span> Dashboard </span>
+                    </a>
+
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="fab fa-accessible-icon "></i>
+                        <span> Patients </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_register_patient.php">Register Patient</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_view_patients.php">View Patients</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_manage_patient.php">Manage Patients</a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="his_admin_discharge_patient.php">Discharge Patients</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_patient_transfer.php">Patient Transfers</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="mdi mdi-cash-multiple "></i>
+                        <span> Doctor </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_add_acc.payable.php">Appointment</a>
+                        </li>
+                    
+                    </ul>
+                </li>
+               
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="fe-file-text"></i>
+                        <span> Patient Records </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_add_medical_record.php">Patient Record</a>
+                        </li>
+                        <li>
+                            <a href="his_admin_manage_medical_record.php">Bed_allocated</a>
+                        </li>
+
+                    </ul>
+                </li>
+
+
+                
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="fas fa-lock"></i>
+                        <span> Password Resets </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="his_admin_manage_password_resets.php">Manage</a>
+                        </li>
+
+                    </ul>
+                </li>
+
+            </ul>
+
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
